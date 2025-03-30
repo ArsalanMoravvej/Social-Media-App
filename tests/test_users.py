ddc8e734e0df006e2db6ba1e820fa296303c477c
ArsalanMoravvej <moravvejmohammad97@gmail.com>
2025-03-30 20:15:58 +0330

    test: remove unused database fixture and enhance user login tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,10 +1,12 @@
+import pytest
 from app import schemas
-from .database import client, session
+from jose import jwt
+from app.config import settings
 
 def test_root(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert response.json() == {'message': 'Welcome to the FastAPI application!'}
+    assert response.json().get('message') == 'Welcome to the FastAPI application!'
 
 def test_create_user(client):
     response = client.post('/users', json={
@@ -15,3 +17,35 @@ def test_create_user(client):
     new_user = schemas.UserResponse(**response.json())
     assert new_user.email == 'johndoe@example.com'
     assert response.status_code == 201
+
+def test_login_user(client, test_user):
+    response = client.post('/login', data={
+        'username': test_user['email'],
+        'password': test_user['password']
+    })
+
+    assert response.status_code == 200
+
+    login_res = schemas.Token(**response.json())
+    payload = jwt.decode(login_res.access_token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+    id = payload.get("user_id")
+
+    assert login_res.token_type == 'bearer'
+    assert id == test_user['id']
+
+@pytest.mark.parametrize("email, password, status_code", [
+    ('wrong_email@example.com','secret', 403),
+    ('johndoe@example.com', 'wrong_password', 403),
+    ('wrong_email@example.com', 'wrong_password', 403),
+    (None, 'secret', 422),
+    ('johndoe@example.com', None, 422)
+])
+def test_incorrect_login(client, test_user, email, password, status_code):
+    login_data = {}
+    if email is not None:
+        login_data['username'] = email
+    if password is not None:
+        login_data['password'] = password
+
+    response = client.post('/login', data=login_data)
+    assert response.status_code == status_code
