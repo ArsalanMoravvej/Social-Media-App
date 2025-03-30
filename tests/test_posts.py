f7a0894505706a557e1de1f6451b61c4aebafcff
ArsalanMoravvej <moravvejmohammad97@gmail.com>
2025-03-30 22:23:42 +0330

    feat: add tests for post creation, deletion, and updates with authorization checks

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -37,9 +37,7 @@ def test_get_one_post_not_exists(authorized_client, test_posts):
         ("New Post", "This is a new post.", True),
         ("Unpublished Post", "UpdThis is an unpublished post.", False),
         ("Pizza Post", "Delicious pizza recipes and tips!", True),
-        ("Scraping Post", "How to scrape data from websites using Python and BeautifulSoup.", True)
-
-    ])
+        ("Scraping Post", "How to scrape data from websites using Python and BeautifulSoup.", True)])
 def test_create_post(authorized_client, test_user, title, content, published):
     response = authorized_client.post("/posts", json={"title": title,
                                                       "content": content,
@@ -52,3 +50,48 @@ def test_create_post(authorized_client, test_user, title, content, published):
     assert post.content == content
     assert post.published == published
     assert post.owner_id == test_user['id']
+
+def test_create_post_default_published(authorized_client, test_user):
+    response = authorized_client.post("/posts", json={"title": "Default Published Post",
+                                                      "content": "This is a default published post."})
+    assert response.status_code == 201
+    post = schemas.PostResponse(**response.json())
+    assert post.published == True
+
+def test_unauthorized_create_post(client):
+    response = client.post("/posts", json={"title": "Unauthorized Post",
+                                                      "content": "This is an unauthorized post.",
+                                                      "published": True})
+    assert response.status_code == 401
+    assert response.json() == {"detail": "Not authenticated"}
+    assert "access_token" not in response.json()
+
+def test_unauthorized_delete_post(client, test_posts):
+    response = client.delete(f"/posts/{test_posts[0].id}")
+    assert response.status_code == 401
+
+def test_delete_post(authorized_client, test_posts):
+    response = authorized_client.delete(f"/posts/{test_posts[0].id}")
+    assert response.status_code == 204
+
+def test_delete_post_not_exists(authorized_client, test_posts):
+    response = authorized_client.delete("/posts/100")
+    assert response.status_code == 404
+
+def test_unauthorized_update_post(client, test_posts):
+    response = client.put(f"/posts/{test_posts[0].id}", json={"title": "Unauthorized Post",
+                                                              "content": "This is an unauthorized post."})
+    assert response.status_code == 401
+
+def test_update_post(authorized_client, test_posts):
+    response = authorized_client.put(f"/posts/{test_posts[0].id}", json={"title": "Updated Post",
+                                                              "content": "This is an updated post."})
+    assert response.status_code == 200
+    post = schemas.PostResponse(**response.json())
+    assert post.title == "Updated Post"
+    assert post.content == "This is an updated post."
+
+def test_update_post_not_exists(authorized_client, test_posts):
+    response = authorized_client.put("/posts/100", json={"title": "Updated Post",
+                                                              "content": "This is an updated post."})
+    assert response.status_code == 404
